437fa0cc1704984aca61c04a85158c64cc6ae3bd
zzzyhhhh210 <3069637798@qq.com>
2026-07-20 13:26:17 +0800

    Add files via upload

---
+++ 2.静态数码管/Nix.h
@@ -0,0 +1,12 @@
+#ifndef __NIX_H_
+#define __NIX_H_
+
+#include "stc8h.h"
+
+typedef     unsigned char   uint_8;
+typedef     unsigned int    uint_16;
+typedef     unsigned long   uint_32;
+
+void Nix_Init(void);
+void Nix_text(uint_8 digit,Number);
+#endif
---
+++ 2.静态数码管/main.c
@@ -0,0 +1,30 @@
+#include "stc8h.h"
+
+#include "Nix.h"
+
+void main(void)
+{	
+	
+	while(1)
+	{
+//œ‘ æ0123 4567
+		Nix_text(1,0);
+		Nix_text(2,1);
+		Nix_text(3,2);
+		Nix_text(4,3);
+		Nix_text(5,4);
+		Nix_text(6,5);
+		Nix_text(7,6);
+		Nix_text(8,7);
+//œ‘ æ02468ACE
+//		Nix_text(1,0);
+//		Nix_text(2,2);
+//		Nix_text(3,4);
+//		Nix_text(4,6);
+//		Nix_text(5,8);
+//		Nix_text(6,10);
+//		Nix_text(7,12);
+//		Nix_text(8,14);
+	}
+
+}
